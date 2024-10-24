702d8f7f53c3b3e8b2332537dc5a6c347aeedc1c
De Almeida Tiago <116738129+Tiagz92@users.noreply.github.com>
2024-10-24 15:15:46 +0200

    stage 4

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,20 +1,5 @@
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-    imgSrc: null,
-  },
-];
-
-function PokemonCard() {
-   
-   const pokemon = pokemonList[0];
-
+function PokemonCard({ pokemon }) {
+  
    return (
     <figure>
       {pokemon.imgSrc ? (
